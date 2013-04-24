a5bdf3f4a3d620e65b159cfcd0be49f00451d80a
Santiago Pavon <santiago@dit.upm.es>
2013-04-24 19:01:03 +0200

    Arreglados errores en comentarios.

--- models/models.js
+++ models/models.js
@@ -24,20 +24,20 @@ var User = sequelize.import(path.join(__dirname,'user'));
 // Relaciones
 
 // La llamada User.hasMany(Post); 
-//  - crea un atributo llamado userId en el modelo de Post  
+//  - crea un atributo llamado UserId en el modelo de Post  
 //  - y en el prototipo de User se crean los metodos getPosts, setPosts,
 //    addPost, removePost, hasPost y hasPosts.
 // 
 // Como el atributo del modelo Post que apunta a User se llama authorId 
-// en vez de userId, he añadido una opcion que lo indica.
+// en vez de UserId, he añadido una opcion que lo indica.
 User.hasMany(Post, {foreignKey: 'authorId'});
 
 // La llamada Post.belongsTo(User);
-//  - crea en el modelo de Post un atributo llamado userId,
+//  - crea en el modelo de Post un atributo llamado UserId,
 //  - y en el prototipo de Post se crean los metodos getUser y setUser.
 //
 // Como el atributo del modelo Post que apunta a User se llama authorId 
-// en vez de userId, he añadido una opcion que lo indica. Asi la 
+// en vez de UserId, he añadido una opcion que lo indica. Asi la 
 // foreignkey del modelo Post es authorId, y los metodos creados son 
 // setAuthor y getAuthor. 
 Post.belongsTo(User, {as: 'Author', foreignKey: 'authorId'});
