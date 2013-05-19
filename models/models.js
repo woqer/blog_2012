eafee0f7d74fff001987f3614830a7c9186dfa43
woqer <woqer88@gmail.com>
2013-05-19 17:16:40 +0200

    ya funciona heroku con postgres

--- models/models.js
+++ models/models.js
@@ -9,11 +9,11 @@ var sequelize = new Sequelize(process.env.DATABASE_NAME,
                               process.env.DATABASE_USER, 
                               process.env.DATABASE_PASSWORD, 
 			      { dialect: process.env.DATABASE_DIALECT, 
-              protocol: process.env.DATABASE_PROTOCOL, 
-              port: process.env.DATABASE_PORT,
+              		protocol: process.env.DATABASE_PROTOCOL, 
+              		port: process.env.DATABASE_PORT,
 			        host: process.env.DATABASE_HOST,
 			        storage: process.env.DATABASE_STORAGE,
-              omitNull: true});
+             		omitNull: true});
 
 // Importar la definicion de la clase Post desde post.js.
 // Y que este modulo exporta la clase Post:
