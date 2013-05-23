f1b46004ec8de08f11497b0f03d2bf3f5681261f
Fido <w.vidana@alumnos.upm.es>
2013-05-23 19:37:47 +0200

    caduca v1

--- caduca.js
+++ caduca.js
@@ -1,2 +1,15 @@
 //Middleware que controla la caducidad de las sesiones
 
+var sessionController = require('./routes/session_controller.js');
+
+function (req, res, next) {
+	var time = new Date().getTime();
+
+	if (req.session.user.time < (time-60000)) {
+		//Cerrar la sesión y notificar por flash
+		sessionController.destroy;
+	} else {
+		//Actualizar el tiempo de sesion
+		req.session.user.time = time;
+	}
+}
